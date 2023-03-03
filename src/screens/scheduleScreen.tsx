15de0d22caaf55d185b3cb71d16ad1f6c9a311c5
Mitchell Joram <mitchell.a.joram@gmail.com>
2023-03-03 01:01:32 -0800

    small fixes

--- src/screens/scheduleScreen.tsx
+++ src/screens/scheduleScreen.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
-import { useNavigation } from '@react-navigation/native';
   
 export const ScheduleScreen = ({ navigation } : any) => {
 
--- src/screens/teamscreen.tsx
+++ src/screens/teamscreen.tsx
@@ -4,6 +4,7 @@ import { useNavigation } from '@react-navigation/native';
 
 export const TeamScreen = () => {
     const navigation = useNavigation();
+    
     return (
       <View className="flex-1 items-center justify-center bg-white">
         <Text className="text-sky-400">Team Screen</Text>
